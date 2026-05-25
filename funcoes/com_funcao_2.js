fc1808beb7f1bf8a13ddd26d29168f688f986146
SaaaS04 <saulo.marcel@souunit.com.br>
2026-05-25 14:33:26 -0300

    Par6ametros/ argumentos em funçoes

---
+++ funcoes/com_funcao_2.js
@@ -0,0 +1,27 @@
+
+const d1 = 3;
+const m1 = 7;
+const a1 = 2021;
+
+function ExibirData1() {
+    console.log(`${d1}/${m1}/${a1}`)
+}
+
+const d2 = 23;
+const m2 = 12;
+const a2 = 2022;
+
+function ExibirData2() {
+    console.log(`${d2}/${m2}/${a2}`)
+}
+function ExibirData(d, m, a) {
+    console.log(`${d}/${m}/${a}`)
+}
+
+ExibirData(d1, m1, a1);
+ExibirData(d1, m1, a1);
+ExibirData(d1, m1, a1);
+
+ExibirData2(d2, m2, a2);
+ExibirData2(d2, m2, a2);
+ExibirData2(d2, m2, a2);
---
+++ funcoes/desafio_funcao.js
@@ -0,0 +1,17 @@
+// funçao: 2 parametros -> console.log(+)
+// funçao: 2 parametros -> console.log(-)
+
+//soma
+function soma(a, b) {
+    console.log(a + b)
+}
+
+function subtracao(c, d) {
+    console.log(c - d)
+}
+
+soma(3, 4)
+soma(31, 98)
+subtracao(11, 7)
+subtracao(19, 52)
+
